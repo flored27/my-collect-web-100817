e31329e6cfc552536c798911bdc5a4607d9951b4
flored27 <dflores@fandm.edu>
2017-09-27 18:47:52 -0400

    easier

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+  len = 0
+  newarray=[]
+  while len < array.length
+    yield array[len]
+    newarray.push(yield array[len])
+    len+=1
+  end
+  return newarray
+  end
